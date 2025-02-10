db5bb1ebe394c20c431d9b9c9cfad323656433ad
Qbeen2 <thdrbqls521@gmail.com>
2025-02-10 11:00:47 +0900

    First Commit

---
+++ src/main/java/org/example/msasbfeed/MsaSbFeedApplication.java
@@ -0,0 +1,13 @@
+package org.example.msasbfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaSbFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaSbFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/msasbfeed/MsaSbFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.msasbfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaSbFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
